00af42c34fc070093b160b3ece2c149db76d38a5
gerodrus <gerodrus@gmail.com>
2017-12-02 13:03:15 +0300

    Started to add FieldCell

---
+++ LD40/Assets/Scripts/Constrollers/FieldCellsController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Assets.Scripts.Core.Field;
+using Constrollers;
+using UnityEngine;
+
+namespace Assets.Scripts.Constrollers
+{
+    class FieldCellsController : BaseController<FieldCellsController>
+    {
+        [SerializeField] private List<BaseFieldCellObject> objects;
+        [SerializeField] private float testWeightInterval = 1.0f;
+
+        private float testWeightTimeRemain;
+
+        public override void AwakeSingleton()
+        {
+        }
+
+        public void Register(BaseFieldCellObject cellObject)
+        {
+            objects.Add(cellObject);
+        }
+
+        public void Remove(BaseFieldCellObject cellObject)
+        {
+            objects.Remove(cellObject);
+        }
+
+        protected virtual void Update()
+        {
+            testWeightTimeRemain -= Time.deltaTime;
+            if (testWeightTimeRemain < 0)
+            {
+                TestWeight();
+                testWeightTimeRemain = testWeightInterval;
+            }
+        }
+
+        private void TestWeight()
+        {
+            for (int i = 0, n = objects.Count; i < n; ++i)
+            {
+                objects[i].TestWeight();
+            }
+        }
+    }
+}
---
+++ LD40/Assets/Scripts/Core/Field/BaseFieldCellObject.cs
@@ -0,0 +1,39 @@
+﻿using Assets.Scripts.Constrollers;
+using UnityEngine;
+
+namespace Assets.Scripts.Core.Field
+{
+    public class BaseFieldCellObject : MonoBehaviour
+    {
+        [Tooltip("Сколько осталось до обрушения")]
+        [SerializeField] private int maxHealth = 5;
+        [SerializeField] private int currentHealth;
+
+        protected virtual void Awake()
+        {
+            FieldCellsController.Instance.Register(this);
+            
+            currentHealth = maxHealth;
+        }
+
+        protected virtual void OnDestroy()
+        {
+            if (!FieldCellsController.WasDestoyed)
+                FieldCellsController.Instance.Remove(this);
+        }
+
+        public virtual void TestWeight()
+        {
+            if (currentHealth <= 0)
+            {
+                DestroyCell();
+            }
+        }
+
+        private void DestroyCell()
+        {
+            // todo
+            Debug.Log("DestroyCell");
+        }
+    }
+}

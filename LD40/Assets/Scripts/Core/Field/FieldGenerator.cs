28d0d98d556297a2dc401312d4ad95e748240738
gerodrus <gerodrus@gmail.com>
2017-12-02 14:43:55 +0300

    Added FieldGenerator

---
+++ LD40/Assets/Scripts/Core/Field/FieldGenerator.cs
@@ -0,0 +1,21 @@
+﻿using Assets.Scripts.Core.Field;
+using UnityEngine;
+
+public class FieldGenerator : MonoBehaviour
+{
+    [SerializeField] private BaseFieldCellObject cellPrefab;
+    [SerializeField] private int width = 1;
+    [SerializeField] private int height = 1;
+
+    private void Start()
+    {
+        for (var x = -width * 0.5f; x < width * 0.5f; ++x)
+        {
+            for (var z = -height * 0.5f; z < height * 0.5f; ++z)
+            {
+                var cell = Instantiate(cellPrefab, transform);
+                cell.transform.localPosition = new Vector3(x, 0.0f, z);
+            }
+        }
+    }
+}

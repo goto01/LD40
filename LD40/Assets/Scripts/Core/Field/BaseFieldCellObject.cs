0486a404a0679ac9a67df4e69a0e0e96a92515b8
gerodrus <gerodrus@gmail.com>
2017-12-03 10:57:01 +0300

    Added FieldCellSettings

--- LD40/Assets/Scripts/Core/Field/BaseFieldCellObject.cs
+++ LD40/Assets/Scripts/Core/Field/BaseFieldCellObject.cs
@@ -6,34 +6,27 @@ namespace Core.Field
 {
     public class BaseFieldCellObject : MonoBehaviour
     {
-        [SerializeField] private float radius = 0.5f;
-        [Header("What weight can be sustained?")]
-        [SerializeField] private float maxWeight = 5;
-        [SerializeField] private float currentWeight;
-        [Header("Settings")]
-        [SerializeField] private float delayBeforeDestroy = 1.0f;
-        [SerializeField] private float weightToPositionRation = 1.0f;
-        [Header("Falling")]
-        [SerializeField] private float fallingAcceleration = -20.0f;
-        [SerializeField] private float minYPosition = -30.0f;
-        [Header("Bouncing")]
-        [SerializeField] private float offsetToAcceleration = 1.0f;
-        [SerializeField] private float springRate = 1.0f;
-        
+        [SerializeField] private FieldCellSettings settings;
+        [SerializeField] private int remainingWeight;
 
         private Coroutine BouncingCoroutine;
 
-        public float WeightDelta { get { return CurrentWeight/maxWeight; } }
+        public float WeightDelta { get { return RemainingWeight / (float) settings.maxWeight; } }
         public bool WasCrashed { get; private set; }
 
         public float Radius
         {
-            get { return radius; }
+            get { return settings.radius; }
         }
 
-        public float CurrentWeight
+        public int RemainingWeight
         {
-            get { return currentWeight; }
+            get { return remainingWeight; }
+        }
+
+        public int CurrentWeight
+        {
+            get { return settings.maxWeight - remainingWeight; }
         }
 
         protected virtual void OnEnable()
@@ -49,36 +42,40 @@ namespace Core.Field
 
         public void ResetWeight()
         {
-            currentWeight = maxWeight;
+            remainingWeight = settings.maxWeight;
         }
 
-        public void AddWeight(float weight)
+        public void AddWeight(int weight)
         {
-            currentWeight -= weight;
+            remainingWeight -= weight;
         }
 
         public virtual void TestWeight()
         {
             if (WasCrashed) return;
             var position = transform.localPosition;
-            if (currentWeight <= 0)
-            {
-                position.y = (currentWeight - maxWeight) * weightToPositionRation;
-                transform.localPosition = position;
-                if (BouncingCoroutine != null)
-                {
-                    StopCoroutine(BouncingCoroutine);
-                    BouncingCoroutine = null;
-                }
-                Invoke("CrashCell", delayBeforeDestroy);
-            }
-            else
+            var currentWeight = CurrentWeight;
+            if (0 == currentWeight)
             {
                 if (null == BouncingCoroutine && position.y < 0.0f)
                 {
                     BouncingCoroutine = StartCoroutine(Bouncing(position));
                 }
                 CancelInvoke("CrashCell");
+            }
+            else 
+            {
+                position.y = -currentWeight * settings.weightToPositionRation;
+                transform.localPosition = position;
+                if (BouncingCoroutine != null)
+                {
+                    StopCoroutine(BouncingCoroutine);
+                    BouncingCoroutine = null;
+                }
+                if (remainingWeight <= 0)
+                {
+                    Invoke("CrashCell", settings.delayBeforeFalling);
+                }
             }
         }
 
@@ -93,10 +90,10 @@ namespace Core.Field
         {
             var position = transform.localPosition;
             var y = position.y;
-            for (var t = 0.0f; minYPosition < position.y; t += Time.deltaTime)
+            for (var t = 0.0f; settings.minYPosition < position.y; t += Time.deltaTime)
             {
                 yield return null;
-                position.y = y + fallingAcceleration * t * t * 0.5f;
+                position.y = y + settings.fallingAcceleration * t * t * 0.5f;
                 transform.localPosition = position;
             }
             gameObject.SetActive(false);
@@ -105,7 +102,7 @@ namespace Core.Field
         private IEnumerator Bouncing(Vector3 startPosition)
         {
             transform.localPosition = startPosition;
-            for (float x = 0, max = -startPosition.y * offsetToAcceleration; x <= max; x += springRate * Time.deltaTime)
+            for (float x = 0, max = -startPosition.y * settings.offsetToAcceleration; x <= max; x += settings.springRate * Time.deltaTime)
             {
                 var p = transform.localPosition;
                 p.y = -Mathf.Sin(x) * startPosition.y * (1.0f - x / max);
---
+++ LD40/Assets/Scripts/Core/Field/FieldCellSettings.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Core.Field
+{
+    [CreateAssetMenu(fileName = "FieldCell Settings", menuName = "Data/Field Cell")]
+    public class FieldCellSettings : ScriptableObject
+    {
+        public float radius = 0.5f;
+        [Header("What weight can be sustained?")]
+        public int maxWeight = 5;
+        [Header("Falling")]
+        public float delayBeforeFalling = 1.0f;
+        public float fallingAcceleration = -20.0f;
+        public float minYPosition = -30.0f;
+        [Header("Bouncing")]
+        public float weightToPositionRation = 1.0f;
+        public float offsetToAcceleration = 1.0f;
+        public float springRate = 1.0f;
+    }
+}

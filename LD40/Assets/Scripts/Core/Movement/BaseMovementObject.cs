40ca263d3d024b5b88134091a186773171276d20
gerodrus <gerodrus@gmail.com>
2017-12-03 18:31:23 +0300

    Added shooted flash effect

--- LD40/Assets/Scripts/Core/Movement/BaseMovementObject.cs
+++ LD40/Assets/Scripts/Core/Movement/BaseMovementObject.cs
@@ -1,4 +1,5 @@
-﻿using Constrollers;
+﻿using System.Collections;
+using Constrollers;
 using Core.Field;
 using UnityEngine;
 
@@ -9,22 +10,33 @@ namespace Core.Movement
         [SerializeField] protected float _speed;
         [SerializeField] private Transform _transform;
         [SerializeField] private Animator _animator;
+        [SerializeField] private SpriteRenderer[] _renderers;
         [SerializeField] private float _weightToSpeedRatio = 1;
+        [SerializeField] private Color _shotFlashColor = Color.black;
+        [SerializeField] private float _shotFlashDuration = 0.5f;
 
         private BaseWeightyObject baseWeightyObject;
         private int RunParameter = Animator.StringToHash("Running");
+        private Coroutine flashCoroutine;
 
         public float Speed
         {
             get
             {
-                return baseWeightyObject == null ? _speed : _speed/baseWeightyObject.CurrentWeight*_weightToSpeedRatio;
+                return baseWeightyObject == null
+                    ? _speed
+                    : _speed / baseWeightyObject.CurrentWeight * _weightToSpeedRatio;
             }
             set { _speed = value; }
         }
+
         protected abstract Vector3 Direction { get; }
-        protected Vector3 Offset { get { return Direction * Speed * Time.deltaTime; } }
-        
+
+        protected Vector3 Offset
+        {
+            get { return Direction * Speed * Time.deltaTime; }
+        }
+
         public virtual void UpdateSelf()
         {
             var offset = Offset;
@@ -32,6 +44,13 @@ namespace Core.Movement
             UpdateAnimator(offset.magnitude);
             if (baseWeightyObject != null)
                 baseWeightyObject.OnMoved(offset);
+        }
+
+        public void OnWasShooted()
+        {
+            if (flashCoroutine != null)
+                StopCoroutine(flashCoroutine);
+            flashCoroutine = StartCoroutine(Flash(_shotFlashColor));
         }
 
         private void UpdateAnimator(float offset)
@@ -50,7 +69,7 @@ namespace Core.Movement
             _transform = transform;
             MovementController.Instance.Register(this);
         }
-        
+
         protected virtual void OnDestroy()
         {
             if (!MovementController.WasDestoyed)
@@ -61,6 +80,24 @@ namespace Core.Movement
         {
             if (!MovementController.WasDestoyed)
                 MovementController.Instance.Remove(this);
+        }
+
+        private IEnumerator Flash(Color startColor)
+        {
+            var finishColor = Color.white;
+            for (float t = 0.0f; t < _shotFlashDuration; t += Time.deltaTime)
+            {
+                foreach (var renderer in _renderers)
+                {
+                    renderer.color = Color.Lerp(startColor, finishColor, t / _shotFlashDuration);
+                }
+                yield return null;
+            }
+            foreach (var renderer in _renderers)
+            {
+                renderer.color = finishColor;
+            }
+            flashCoroutine = null;
         }
     }
 }

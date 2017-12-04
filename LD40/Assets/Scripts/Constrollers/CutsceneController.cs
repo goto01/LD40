935ce40e94ef3630114fcb6d5162de4c7674b4fa
gerodrus <gerodrus@gmail.com>
2017-12-04 21:33:57 +0300

    Added cutscene

---
+++ LD40/Assets/Scripts/Constrollers/CutsceneController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Controllers;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class CutsceneController : MonoBehaviour
+{
+    public int nextSceneId = 2;
+    public Image image;
+    public Color endColor;
+    public Sprite[] slides;
+
+    int index;
+    
+    private string FadeInParameter = "FadeIn";
+    private string FadeOutParameter = "FadeOut";
+
+    private void Start()
+    {
+        EffectController.Instance.FadeIn();
+    }
+
+    public void NextSlide()
+    {
+        ++index;
+        if (index < slides.Length)
+            image.sprite = slides[index];
+        else
+            NextScene();
+    }
+
+    public void NextScene()
+    {
+        image.sprite = null;
+        image.color = endColor;
+        EffectController.Instance.FadeOut();
+        Invoke("SwitchToNextScene", EffectController.Instance.FadeDuration);
+    }
+
+    private void SwitchToNextScene()
+    {
+        SceneManager.LoadScene(nextSceneId);
+    }
+}
